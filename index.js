de9fc48e6410b6e430afecbacc5b75348bc01172
GonzaloHernandezHernandez <145341185+GonzaloHernandezHernandez@users.noreply.github.com>
2025-05-26 11:09:15 +0200

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const questionsApi = require('./routes/questions');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 (async () => {
   try {
